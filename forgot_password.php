58124f4df7f7c44a339a88c98d58b413530f8617
smtharun2007-coder <smtharun2007@gmail.com>
2026-06-18 11:39:18 +0530

    About page - show only guidelines in All Semester Analysis

--- forgot_password.php
+++ forgot_password.php
@@ -237,7 +237,7 @@ $portal_type = isset($_GET['portal']) ? $_GET['portal'] : 'student';
     <h1>Student Growth Index</h1>
     <?php if ($portal_type === 'mentor'): ?>
         <div class="portal-badge mentor-portal">👨 Mentor Portal</div>
-    <?php
+
     <?php endif; ?>
     <h2>Forgot Password</h2>
 
